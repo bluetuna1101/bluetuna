d0c1a5b8260872f9e9f397a8a1a9348feae42536
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-03-09 22:37:20 +0900

    *click 시 요소 추가해주는 함수

---
+++ HTML_CSS/study/vue/elementadd.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li @class="createElemental">li 추가하기</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  methods: {
+    createChat(e) {
+      let chatBubble = document.createElement("li")
+      chatBubble.classList.add("user_input")
+      chatBubble.innerHTML = e.target.innerText
+      console.log(chatBubble)
+      document.querySelector(".chatlist").appendChild(chatBubble)
+    },
+  },
+}
+</script>
+
+<style></style>
