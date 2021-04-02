d70a61097c19c98699df8f7c8fc3f6bfea39d4c2
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-04-02 23:31:44 +0900

    *vue watch route

---
+++ HTML_CSS/study/vue/route.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  watch: {
+    $route(to, from) {
+      // 경로 변경에 반응하여... to 부터 from 까지 움직임을 항상 지켜본다.
+      // 페이지 이동시에 호출할 함수를 적어주면 페이지 이동시마다 함수가 실행된다.
+    },
+  },
+}
+</script>
+
+<style></style>
