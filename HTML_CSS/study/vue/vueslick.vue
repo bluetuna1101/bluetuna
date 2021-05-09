df20ed6d15467e413d74c4c12b8e469293cd253d
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-05-09 16:16:28 +0900

    *vue slick

---
+++ HTML_CSS/study/vue/vueslick.vue
@@ -0,0 +1,25 @@
+<template>
+  <VueSlickCarousel v-bind="settings">
+    <div><h3>1</h3></div>
+    /*...*/
+  </VueSlickCarousel>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      settings: {
+        arrows: true, //화살표
+        dots: true, //
+        infinite: true, // 무한
+        slidesToShow: 3, // 보여지는 갯수 *(height에 영향을 줌)
+        slidesToScroll: 1, // 스크롤되는 수
+        vertical: true, // 세로 슬라이드
+        verticalSwiping: true,
+        pauseOnFocus: false, // 자연스러운 autoplay 를 위해서는 아래 두 값을 false 로 줘야 한다.
+        pauseOnHover: false,
+      },
+    }
+  },
+}
+</script>
